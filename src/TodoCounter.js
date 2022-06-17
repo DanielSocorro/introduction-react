7051e4763a125ccdec42fb1821cda203261ceaa3
Daniel Socorro <dansc147@Developer.local>
2022-06-17 15:27:20 -0400

    Taken course

---
+++ src/TodoCounter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './TodoCounter.css';
+
+
+function TodoCounter() {
+    return(
+        <h2 className='TodoCounter'>Completed 0 of 4 Sings</h2>
+    )
+ }
+
+ export { TodoCounter };
